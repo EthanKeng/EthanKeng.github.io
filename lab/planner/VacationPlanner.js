0c882a5ae564c338dbde396de2c9d3fd1b7f4033
EthanKeng <yucheng.keng@gmail.com>
2018-07-21 11:26:30 +0900

    section2

--- lab/planner/VacationPlanner.js
+++ lab/planner/VacationPlanner.js
@@ -17,5 +17,18 @@ function calculateDays(){
     else{
         daysMessage.innerHTML = "You'll have plenty of time to relax and have fun!";
     }
-
+    var hours = days * 24;
+    var minutes = hours * 60; 
+    var seconds = minutes * 60;
+    var hoursMessage = document.getElementById("hoursMessage");
+    var minutesMessage = document.getElementById("minutesMessage"); 
+    var secondsMessage = document.getElementById("secondsMessage");
+    hoursMessage.innerHTML = "That means you'll be traveling for " + hours + " hours.";
+    minutesMessage.innerHTML = "Which means you'll be traveling for " + minutes + "minutes.";
+    secondsMessage.innerHTML = "That means you'll be traveling for " + seconds + "seconds.";
+    var timingNext = document.getElementById("timingNext");
+    timingNext.removeAttribute("hidden");
 }
+timingNext.onclick = function(){
+    document.getElementById("budgetSection").removeAttribute("hidden");
+} 
