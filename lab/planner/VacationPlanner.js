035b5f3f8b20381c35360af8f4f9a668c1dee2b3
EthanKeng <yucheng.keng@gmail.com>
2018-07-21 14:38:52 +0900

    end1

--- lab/planner/VacationPlanner.js
+++ lab/planner/VacationPlanner.js
@@ -32,3 +32,32 @@ function calculateDays(){
 timingNext.onclick = function(){
     document.getElementById("budgetSection").removeAttribute("hidden");
 } 
+
+var budgetInput = document.getElementById("tripBudget");
+var enterBudget = document.getElementById('enterBudget') ;
+enterBudget.onclick = x;
+function x(){
+    var daysInput = document.getElementById("days");
+    var days = daysInput.value;
+    var budget = budgetInput.value;
+    var ntd = budget * 30.59 ;
+    var tripExchangeMsg = document.getElementById("tripExchangeMsg");
+    var dailyExchangeMsg = document.getElementById("dailyExchangeMsg"); 
+    var exchange10Msg = document.getElementById("exchange10Msg");
+    var exchange500Msg = document.getElementById('exchange500Msg');
+    tripExchangeMsg.innerHTML = "This means you'll need "+ ntd + "NTD for your trip."
+    dailyExchangeMsg.innerHTML = "That means you can spend " + ntd/days + 'NTD per day.'
+    exchange10Msg.innerHTML = "You'll need "+ 10*budget + "NTD for an item that would cost you 10USD."
+    exchange500Msg.innerHTML = "You'll need "+ 500*budget + "NTD for an item that would cost you 500USD."
+}
+
+var images = ["images/Honeymoon-in-Taiwan.jpg", "images/Jiufen-taiwan-travel-photography-jake-and-dannie-26.jpg", "images/keelung-taiwan.jpg", "images/taiwan-mountains-xlarge.jpg" ];
+var currentImage = 0;
+setInterval(changeImage, 7000);
+function changeImage(){
+    currentImage++;
+    if(currentImage > images.length - 1){
+        currentImage = 0;
+    }
+    document.body.style.backgroundImage = "url(" + images[currentImage] + ")";
+}
