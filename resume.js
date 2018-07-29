19622dadd0ee57f142df321f0d5cb3623fa40abe
EthanKeng <yucheng.keng@gmail.com>
2018-07-29 18:17:08 +0900

    ITI picture JS/CSS updated

--- resume.js
+++ resume.js
@@ -24,3 +24,17 @@ function func4()
 {  
     document.getElementById("hover-img2").setAttribute("style", "display:none;")
 }
+
+var item3 = document.getElementById("ITI");
+item3.addEventListener("mouseover", func5, false);
+item3.addEventListener("mouseout", func6, false);
+
+function func5()
+{   
+   document.getElementById("img10").setAttribute("style", "display:block;")
+}
+
+function func6()
+{  
+    document.getElementById("img10").setAttribute("style", "display:none;")
+}
